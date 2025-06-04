5b1f58290a6dc360dccdc9e30d288d6d8b8d2ae7
Maris Galesloot <maris.galesloot@ru.nl>
2025-06-04 18:18:09 +0100

    Add stuff

--- test_reach_prob.py
+++ test_reach_prob.py
@@ -17,6 +17,8 @@ import multiprocessing
 from config import *
 
 
-env = f"{BASE_SKETCH_DIR}/rocks-4-1"
+# env = f"{BASE_SKETCH_DIR}/rocks-4-1"
+# env = f"{BASE_SKETCH_DIR}/obstacles-demo"
+env = f"{BASE_SKETCH_DIR}/avoid-8-2-easy"
 gd = POMDPFamiliesSynthesis(env, use_softmax=True, steps=1, learning_rate=0.01, use_momentum=True, seed=0)
-fsc, value = gd.run_gradient_descent_on_family(1000, 2, timeout=600, memory_model=None, random_selection=False)
+fsc, value = gd.run_gradient_descent_on_family(1000, num_nodes=1, timeout=600, memory_model=None, random_selection=False)
